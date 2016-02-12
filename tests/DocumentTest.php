3c3f195ec6f12f8a1b3c1b20b42cae01ee2506b7
Genaro Madrid <genmadrid@gmail.com>
2016-02-12 01:15:12 -0600

    test properties

--- tests/DocumentTest.php
+++ tests/DocumentTest.php
@@ -6,14 +6,42 @@ use Mifiel\Document;
 
 class DocumentTest extends \PHPUnit_Framework_TestCase {
 
-  public function testAll() {
+  const ORIGINAL_HASH = '7cf3c80bbe595734a960b49a79d6e87f8932c21fea6665b69cfa1257f85f7dc1';
+
+  public function setTokens() {
     ApiClient::setTokens(
       '44c783d37ef12d3912f911c7b3ac44d657d83b17',
       'm7MvN0kvmF4/TbYGb7ImlWtUbfQ2XSj+STzvmLBCzOI2L+Kgr2ajaOkftQevv8/KJILevxlpvFWpVbj7hczQQg=='
     );
+  }
+
+  public function getDocument() {
+    $this->setTokens();
+    $documents = Document::all();
+    return $documents[0];
+  }
+
+  public function testAll() {
+    $this->setTokens();
     $documents = Document::all();
     $this->assertTrue(is_array($documents));
-    $this->assertTrue(get_class($documents[0]) == 'Mifiel\Document');
+    $this->assertEquals('Mifiel\Document', get_class($documents[0]));
+  }
+
+  public function testGetProperties() {
+    $document = $this->getDocument();
+    $id = '708c8b9d-5440-46ef-8b67-5297aaf058d5';
+    $this->assertEquals($id, $document->id);
+  }
+
+  public function testSetProperties() {
+    $document = $this->getDocument();
+    $this->assertEquals(self::ORIGINAL_HASH, $document->original_hash);
+
+    $original_hash = 'blah';
+    $document->original_hash = $original_hash;
+    $this->assertEquals($original_hash, $document->original_hash);
+  }
   }
 
 }
